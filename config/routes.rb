ac9e21873f58ac78f10ae27b36fc8bbc4d7bcbde
nuoroute <nuoroute@gmail.com>
2016-10-23 19:39:47 +0300

    Fixed some links and routes, started remapping users_controller actions to artists_controller in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,83 +1,29 @@
-Rails.application.routes.draw do  
+Rails.application.routes.draw do 
 
-  # General:
-  resources :users do
-    resources :feeds
-  end
-
-  resources :videos
-
-  # resources :artists do 
-  #   resources :feeds
-  # end
-
-  get 'artists/:id' => 'artists#show'
-  get 'artists/' => 'artists#index'
-  get 'users/' => 'artists#index'
-  get 'users/:id' => 'artists#show'
-
-  # Custom:
+  # Root
   root 'pages#home'
-  get '/home' => 'pages#home'
-  get '/about' => 'pages#about'
-  get '/support' => 'pages#support'
-  # get '*path' => redirect('/') # Messes up Devise's redirection to Log In / Sign Up
 
   # Devise:
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
+
+    # Map users_controller (Devise) to artists_controller
+    get '/users/' => 'artists#index'
+    get '/users/:id' => 'artists#show'
   end
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # Resources:
+  resources :users do  # Don't place this above Devise's routes
+    resources :feeds
+  end
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  resources :videos
+  resources :artists
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  # Custom (pages):
+  get '/home' => 'pages#home'
+  get '/about' => 'pages#about'
+  get '/support' => 'pages#support'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
