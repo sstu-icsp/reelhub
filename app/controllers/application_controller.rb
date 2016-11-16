4406a312e001ce23be84340cb8efbbfe6056d91d
nuoroute <nuoroute@gmail.com>
2016-11-16 21:03:51 +0300

    Added avatar to User model (Paperclip)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-	
+  # Paperclip parameters permit for User model
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :avatar])
+  	devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password, :avatar])
+  end
 end
